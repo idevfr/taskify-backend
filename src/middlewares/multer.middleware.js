4825a1a13f4f2ed2c337a556a6f0242761c81a8b
=idvfr <=utubediscow@gmail.com>
2026-01-22 22:02:28 +0600

    initial commit

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -2,7 +2,10 @@ import multer from "multer";
 import { fileURLToPath } from "url";
 import { dirname, resolve } from "path";
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const destinationDir = resolve(__dirname, "../../../backend/public/temp");
+const destinationDir =
+  process.env.NODE_ENV === "development"
+    ? resolve(__dirname, "../../../backend/public/temp")
+    : "/temp/upload";
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, `${destinationDir}`);
