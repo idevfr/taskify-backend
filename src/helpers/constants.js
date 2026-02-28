9bfdc6d8482db00da8eb69f4c634e08e155d66cc
idevfr <=utubediscow@gmail.com>
2026-02-28 14:55:59 +0600

    updated cookies config

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -2,12 +2,12 @@ export const DB_NAME = "taskify";
 export const accessCookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "lax",
+  sameSite: "none",
   maxAge: 24 * 60 * 60 * 1000,
 };
 export const refreshCookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "lax",
+  sameSite: "none",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
